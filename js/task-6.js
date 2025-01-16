7fcd34a789589f527f89b79ae9be2f706aa8ac60
Val_M <v.mischenko90@gmail.com>
2025-01-16 13:50:04 +0200

    change task-6 implementation

--- js/task-6.js
+++ js/task-6.js
@@ -9,32 +9,22 @@ const createBoxesBtn = document.querySelector('button[data-create');
 const destroyBoxesBtn = document.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
 
-const validateInput = () => {
-  const inputValue = Number(userInput.value);
-
-  if (inputValue < 1 || inputValue > 100) {
-    createBoxesBtn.disabled = true;
-  } else {
-    createBoxesBtn.disabled = false;
-  }
-};
-
 const handleGenerateBoxes = () => {
   const inputValue = Number(userInput.value);
-
-  handleDestroyBoxes();
-  const fragment = document.createDocumentFragment();
-  let size = 30;
-  for (let i = 1; i <= inputValue; i += 1) {
-    const box = document.createElement('div');
-    box.style.backgroundColor = getRandomHexColor();
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    fragment.appendChild(box);
-    size += 10;
+  if (inputValue < 1 || inputValue > 100) {
+    return;
   }
 
-  boxes.appendChild(fragment);
+  handleDestroyBoxes();
+  const markup = Array.from({ length: inputValue }).reduce((acc, _, index) => {
+    const size = 30 + index * 10;
+    return (
+      acc +
+      `<div style="background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px"></div>`
+    );
+  }, '');
+
+  boxes.insertAdjacentHTML('beforeend', markup);
   userInput.value = '';
 };
 
@@ -42,6 +32,5 @@ const handleDestroyBoxes = () => {
   boxes.innerHTML = '';
 };
 
-userInput.addEventListener('input', validateInput);
 createBoxesBtn.addEventListener('click', handleGenerateBoxes);
 destroyBoxesBtn.addEventListener('click', handleDestroyBoxes);
